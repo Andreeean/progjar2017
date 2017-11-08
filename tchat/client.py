3075d9169179005012e8726b9375a3e230219068
Andrean Januar <an.jp29@gmail.com>
2017-11-08 14:55:10 +0700

    tchat

---
+++ tchat/client.py
@@ -0,0 +1,78 @@
+import socket
+
+import sys
+
+import threading
+
+import os
+
+BUFFER = 1024
+HOST = "localhost"
+PORT = 65535
+
+sock = socket.socket()
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.connect((HOST, PORT))
+username = None
+
+
+def user_input():
+    while True:
+        msg = sys.stdin.readline()[:-1]
+
+        if username is None:
+            ''' username prompt '''
+            sock.send(('/username ' + msg).encode())
+
+        else:
+            if msg.startswith('/file'):
+                _split = msg.split(' ')
+                to = _split[1]
+                filename = _split[2]
+                path = os.path.join(username, filename)
+                size = os.path.getsize(path)
+
+                sock.send(('%s %d' % (msg, size)).encode())
+                with open(path, 'rb') as fuck:
+                    sock.send(fuck.read())
+                print('File sent (%s:%d) to %s. ' % (filename, size, to))
+
+            else:
+                sock.send(msg.encode())
+
+
+threading.Thread(target=user_input).start()
+
+while True:
+    request = sock.recv(BUFFER).decode()
+
+    if request:
+        if request.startswith('/username'):
+            _split = request.split(' ')
+            username = _split[1]
+            if not os.path.exists(username):
+                os.makedirs(username)
+
+        elif request.startswith('/file'):
+            _split = request.split(' ')
+            sender = _split[1]
+            filename = _split[2]
+            size = int(_split[3])
+            path = os.path.join(username, filename)
+
+            with open(path, 'wb') as fuck:
+                while size > 0:
+                    piece = sock.recv(min(BUFFER, size))
+                    fuck.write(piece)
+                    size -= BUFFER
+            print('%s sent a file (%s).' % (sender, filename))
+
+        else:
+            print(request)
+
+    else:
+        break
+
+sock.close()
+if os.path.exists(username):
+    os.removedirs(username)
---
+++ tchat/server.py
@@ -0,0 +1,89 @@
+import socket as pysocket
+
+import select
+
+import re
+
+import os
+
+BUFFER = 1024
+HOST = "localhost"
+PORT = 65535
+SERVER_MEDIA_PATH = 'server/media'
+
+server_socket = pysocket.socket()
+server_socket.setsockopt(pysocket.SOL_SOCKET, pysocket.SO_REUSEADDR, 1)
+server_socket.bind((HOST, PORT))
+server_socket.listen()
+sockets = [server_socket]
+ftp_sockets = []
+clients = {}
+usernames = {}
+
+if not os.path.exists(SERVER_MEDIA_PATH):
+    os.makedirs(SERVER_MEDIA_PATH)
+
+print('Server ready. Port: %d' % PORT)
+
+while True:
+    read_ready, write_ready, exception = select.select(sockets, [], [])
+
+    for sock in read_ready:
+        if sock is server_socket:
+            socket, address = sock.accept()
+            sockets.append(socket)
+            socket.send(b'Enter your username!')
+        else:
+            request = sock.recv(BUFFER).decode()
+            print(request)
+
+            if request:
+                if request.startswith('@'):
+                    _split = re.split('[@ ]', request)
+                    username = _split[1]
+
+                    if username in clients.keys():
+                        receiver = clients[username]
+                        sender = usernames[sock]
+                        message = _split[2]
+                        receiver.send(('%s: %s' % (sender, message)).encode())
+                    else:
+                        sock.send(b'Username tidak ada cok!')
+
+                elif request.startswith('/username'):
+                    username = request.split(' ')[1]
+
+                    if username not in clients.keys():
+                        clients[username] = sock
+                        usernames[sock] = username
+                        sock.send(('/username ' + username).encode())
+                    else:
+                        sock.send(b'Username udah dipake!')
+
+                elif request.startswith('/file'):
+                    _split = request.split(' ')
+                    receiver = clients[_split[1]]
+                    filename = _split[2]
+                    size = int(_split[3])
+                    sender = usernames[sock]
+                    path = os.path.join(SERVER_MEDIA_PATH, filename)
+
+                    receiver.send(('/file %s %s %d' % (sender, filename, size)).encode())
+                    with open(path, 'wb') as fuck:
+                        while size > 0:
+                            piece = sock.recv(min(BUFFER, size))
+                            receiver.send(piece)
+                            size -= BUFFER
+                    print('%s sent a file (%s:%d) to %s.' % (sender, filename, size, usernames[receiver]))
+
+                else:
+                    for username, socket in clients.items():
+                        socket.send(('%s: %s' % (usernames[sock], request)).encode())
+
+            else:
+                ''' a user disconnected '''
+                print('%s disconnected.' % usernames[sock])
+                sock.close()
+                clients.pop(usernames[sock])
+                usernames.pop(sock)
+                sockets.remove(sock)
